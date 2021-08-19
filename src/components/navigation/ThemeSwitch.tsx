529470126935ac49b73f0c9a2762fe52104d5f27
Aditya Pratap <adityapratap7344@gmail.com>
2021-08-20 02:21:02 +0530

    feat: added heroicons

--- src/components/navigation/ThemeSwitch.tsx
+++ src/components/navigation/ThemeSwitch.tsx
@@ -1,3 +1,4 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import { useAppThemeContext } from "../../contexts/AppTheme.context";
 import { ThemeMode } from "../../hooks/appTheme.hook";
 
@@ -9,18 +10,25 @@ export const ThemeSwitch = (_props: Props) => {
     const isDarkMode = state.mode === ThemeMode.DARK;
 
     return (
-        <div className="flex justify-between items-center gap-1">
+        <div className="flex justify-between items-center gap-2">
             <div
-                className={`w-12 h-8 flex items-center ${
+                className={`w-10 h-6 sm:w-12 sm:h-8 flex items-center ${
                     isDarkMode ? "bg-primary" : "bg-neutral"
                 } rounded-full p-1 cursor-pointer`}
                 onClick={toggleMode}
             >
                 <div
-                    className={`bg-neutralBg w-6 h-6 rounded-full shadow-md transform ${
+                    className={`bg-neutralBg w-4 h-4 sm:w-6 sm:h-6 rounded-full shadow-md transform ${
                         isDarkMode && "translate-x-4"
                     } duration-300 ease-in-out`}
-                ></div>
+                />
+            </div>
+            <div className="flex justify-center items-center">
+                {isDarkMode ? (
+                    <MoonIcon className="h-6 sm:h-8 text-onNeutralBg" />
+                ) : (
+                    <SunIcon className="h-7 sm:h-9 text-yellow-500" />
+                )}
             </div>
         </div>
     );

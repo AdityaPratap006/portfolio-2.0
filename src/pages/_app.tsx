7a9e1350cc1808e74d40ce084368cbd90bbc7a05
Aditya Pratap <adityapratap7344@gmail.com>
2021-08-19 20:50:33 +0530

    feat: added sidebar and bg image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from "next/app";
+import { Sidebar } from "../components/navigation/Sidebar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <div className="app-container w-screen min-h-screen">
+            <div className="w-screen min-h-screen flex flex-col justify-start items-center">
+                <Sidebar />
+                <main className="content-container w-screen min-h-screen flex flex-col justify-start items-center sm:ml-20 lg:w-5/6 md:ml-auto">
+                    <Component {...pageProps} />
+                </main>
+            </div>
+        </div>
+    );
 }
 export default MyApp;

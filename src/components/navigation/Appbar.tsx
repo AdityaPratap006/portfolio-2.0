326654c395d3ffd4a6913afe69d33b9a09a381d6
Aditya Pratap <adityapratap7344@gmail.com>
2021-08-22 13:28:26 +0530

    feat: added github profile link

--- src/components/navigation/Appbar.tsx
+++ src/components/navigation/Appbar.tsx
@@ -1,3 +1,4 @@
+import NextImage from "next/image";
 import { useRouter } from "next/router";
 import { navRoutes } from "../../utils/navRoutes";
 import { ThemeSwitch } from "./ThemeSwitch";
@@ -17,7 +18,7 @@ export const Appbar = (_props: Props) => {
             z-40 bg-neutralBg
             border-b border-gray-600
             bg-opacity-40 backdrop-filter backdrop-blur-xl 
-            flex  justify-start items-center 
+            flex  justify-start items-center gap-2 sm:gap-4
             pl-4 pr-4"
         >
             <h4 className=" text-primaryBold font-extrabold text-2xl sm:text-4xl p-0 m-0 capitalize">
@@ -25,6 +26,20 @@ export const Appbar = (_props: Props) => {
             </h4>
             <div className="ml-auto">
                 <ThemeSwitch />
+            </div>
+            <div className="flex justify-center items-center p-1 bg-gray-100 rounded-full">
+                <a
+                    href={`https://github.com/AdityaPratap006`}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="w-8 h-8  relative"
+                >
+                    <NextImage
+                        src={"/github.svg"}
+                        alt="Github Icon"
+                        layout="fill"
+                    />
+                </a>
             </div>
         </nav>
     );
